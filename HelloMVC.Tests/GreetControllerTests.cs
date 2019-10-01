19d79c688ec2ae06d85e03905ebed48249d923eb
Steven <steventruong1995@yahoo.com>
2019-10-01 15:28:29 -0400

    greeting name test

--- HelloMVC.Tests/GreetControllerTests.cs
+++ HelloMVC.Tests/GreetControllerTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using HelloMVC.Controllers;
 using Xunit;
+using HelloMVC.Models;
 
 namespace HelloMVC.Tests
 {
@@ -19,6 +20,28 @@ namespace HelloMVC.Tests
 
                 Assert.IsType<ViewResult>(result);
             }
-        }
+
+            [Fact]
+            public void Index_Passes_GreetingModel_To_View()
+            {
+                var controller = new GreetController();
+
+                var result = controller.Index("ThisIsAString");
+
+                Assert.IsType<GreetingModel>(result.Model);
+            }
+
+            [Fact]
+            public void Index_Sets_Name_On_Model()
+            {
+                var expectedName = "ExampleString";
+                var controller = new GreetController();
+
+                var result = controller.Index(expectedName);
+
+                var model = (GreetingModel)result.Model;
+                Assert.Equal(expectedName, model.Name);
+            }
+    }
     
 }
--- HelloMVC/Controllers/GreetController.cs
+++ HelloMVC/Controllers/GreetController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloMVC.Controllers
 {
     public class GreetController : Controller
     {
+        //public ViewResult Index(string name)
+        //{
+        //    return View();
+        //}
+
+        //public ViewResult Index(string name)
+        //{
+        //    var model = new GreetingModel();
+        //    return View(model);
+        //}
+
         public ViewResult Index(string name)
         {
-            return View();
+            var model = new GreetingModel();
+            model.Name = name;
+            return View(model);
         }
     }
 }
